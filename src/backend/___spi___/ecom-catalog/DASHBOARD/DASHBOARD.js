73c4065ef9008bc735c338b91dcaf1dd3ce8308e
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2023-01-26 15:53:00 +0000

    Initial commit

---
+++ src/backend/___spi___/ecom-catalog/DASHBOARD/DASHBOARD.js
@@ -0,0 +1,17 @@
+import * as ecomCatalog from 'interfaces-ecommerce-v1-catalog-provider';
+
+/**
+ * Retrieves specified item data from a specified catalog.
+ * @param {import('interfaces-ecommerce-v1-catalog-provider').GetCatalogItemsOptions} options
+ * @param {import('interfaces-ecommerce-v1-catalog-provider').Context} context
+ * @returns {Promise<import('interfaces-ecommerce-v1-catalog-provider').GetCatalogItemsResponse>}
+ */
+export const getCatalogItems = async (options, context) => {};
+
+/**
+ * Retrieves specified item data from a specified catalog.
+ * @param {import('interfaces-ecommerce-v1-catalog-provider').QueryCatalogItemsOptions} options
+ * @param {import('interfaces-ecommerce-v1-catalog-provider').Context} context
+ * @returns {Promise<import('interfaces-ecommerce-v1-catalog-provider').QueryCatalogItemsResponse>}
+ */
+export const queryCatalogItems = async (options, context) => {};
---
+++ src/pages/Learn More.x0rw6.js
@@ -0,0 +1,13 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MVGM2F6');</script>
+<!-- End Google Tag Manager -->
+
+});
